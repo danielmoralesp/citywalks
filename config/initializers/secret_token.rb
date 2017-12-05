20872a717401104f5e1c47962ba2ebd28ec6d68a
danielmoralesp <daniel_moralesp@hotmail.com>
2017-12-05 07:45:42 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,19 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+
+# Although this is not needed for an api-only application, rails4 
+# requires secret_key_base or secret_token to be defined, otherwise an 
+# error is raised.
+# Using secret_token for rails3 compatibility. Change to secret_key_base
+# to avoid deprecation warning.
+# Can be safely removed in a rails3 api-only application.
+Citywalks::Application.config.secret_token = 'c12515daf569dd929861f1245cffe555d5d413914a77657ba39c3cc126edfe4f017d1484d63edff431c6f52af9d1869d1fcda1b233ee888be94ad088ffb43e26'
